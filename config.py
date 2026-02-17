ab634f488d46a1c6d95167d79a214b3b0937bcf4
projects-sn <vladapv65@gmail.com>
2026-02-17 15:16:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,9 @@ import os
 from pathlib import Path
 
 BASE = Path(__file__).resolve().parent
-RAG_INDEX = BASE / "rag_index"
-DOCUMENTS_JSON = RAG_INDEX / "documents.json"
-FAISS_INDEX = RAG_INDEX / "index.faiss"
-CONFIG_JSON = RAG_INDEX / "config.json"
+DOCUMENTS_JSON = "documents.json"
+FAISS_INDEX = "index.faiss"
+CONFIG_JSON = "config.json"
 
 # Embedding model
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
